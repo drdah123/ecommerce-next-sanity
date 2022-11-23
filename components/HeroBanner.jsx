c0f01a9c8e1b9158143eda68a2d3534b7d64ba12
drdah123 <108929184+drdah123@users.noreply.github.com>
2022-11-23 17:53:32 +0300

    ss

--- components/HeroBanner.jsx
+++ components/HeroBanner.jsx
@@ -6,24 +6,27 @@ const HeroBanner = ({ heroBanner }) => {
   return (
     <div className="hero-banner-container">
       <div className="hero-banner-container-min">
-        <p className="beats-solo">{heroBanner.smallText}</p>
-        <h3>{heroBanner.midText}</h3>
-        <h1>{heroBanner.largeText1}</h1>
+        <p className="beats-solo">{heroBanner?.smallText}</p>
+        <h3>{heroBanner?.midText}</h3>
+        <h1>{heroBanner?.largeText1}</h1>
         <img
-          src={urlFor(heroBanner.image)}
+          src={urlFor(heroBanner?.image)}
           alt="tesla s plaid"
           className="hero-banner-image"
         />
-
+        {console.log(
+          '🚀 ~ file: HeroBanner.jsx ~ line 6 ~ HeroBanner ~ heroBanner',
+          heroBanner
+        )}
         <div className="hero-banner-product">
-          <Link href={`/product/${heroBanner.product}`}>
-            <button type="button" className="hero-button-text">
-              {heroBanner.buttonText}
+          <Link href={`/product/${heroBanner?.slug}`}>
+            <button type="button" className="hero-button-text" disabled>
+              {heroBanner?.buttonText}
             </button>
           </Link>
           <div className="desc ">
             <h5>description</h5>
-            <p>{heroBanner.desc}</p>
+            <p>{heroBanner?.desc}</p>
           </div>
         </div>
       </div>
--- pages/index.js
+++ pages/index.js
@@ -1,39 +1,40 @@
-import React from 'react'
-import { Footer, FooterBanner, HeroBanner, Product } from '../components'
-import { client } from '../lib/Client'
+import React from 'react';
+import { Footer, FooterBanner, HeroBanner, Product } from '../components';
+import { client } from '../lib/Client';
 
 const Home = ({ products, bannerData }) => {
   return (
     <>
       <HeroBanner heroBanner={bannerData.length && bannerData[0]} />
 
-
-      <div className='products-heading'>
+      <div className="products-heading">
         <h2>best Saller Products</h2>
         <p>good</p>
       </div>
 
-      <div className='products-container'>
-        {products?.map(product => <Product key={product._id} product={product} />)}
+      <div className="products-container">
+        {products?.map((product) => (
+          <Product key={product._id} product={product} />
+        ))}
       </div>
       <FooterBanner footerBanner={bannerData && bannerData[0]} />
     </>
-  )
-}
+  );
+};
 
 export const getServerSideProps = async () => {
-  const query = '*[_type=="product"]'
-  const products = await client.fetch(query)
+  const query = '*[_type=="product"]';
+  const products = await client.fetch(query);
 
-  const bannerQuery = '*[_type == "banner"]'
-  const bannerData = await client.fetch(bannerQuery)
+  const bannerQuery = '*[_type == "banner"]';
+  const bannerData = await client.fetch(bannerQuery);
 
   return {
     props: {
       products,
-      bannerData
-    }
-  }
-}
+      bannerData,
+    },
+  };
+};
 
-export default Home
+export default Home;
